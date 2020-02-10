58e0d8c16783426c6a4b8508930c0585341b909c
Elan Ruusamäe <glen@pld-linux.org>
2020-02-11 00:29:15 +0200

    Re-format ProfilerInterface with PSR-2

--- src/Profilers/ProfilerInterface.php
+++ src/Profilers/ProfilerInterface.php
@@ -2,27 +2,28 @@
 
 namespace Xhgui\Profiler\Profilers;
 
-interface ProfilerInterface {
-	/**
-	 * Enable the specific profiler
-	 *
-	 * @param array $flags
-	 * @param array $options
-	 */
-	public function enableWith($flags = array(), $options = array());
+interface ProfilerInterface
+{
+    /**
+     * Enable the specific profiler
+     *
+     * @param array $flags
+     * @param array $options
+     */
+    public function enableWith($flags = array(), $options = array());
 
-	/**
-	 * Disable (stop) the profiler. Return the collected data
-	 *
-	 * @return array
-	 */
-	public function disable();
+    /**
+     * Disable (stop) the profiler. Return the collected data
+     *
+     * @return array
+     */
+    public function disable();
 
-	/**
-	 * Map generic Xhgui\Profiler\ProfilingFlags to {SPECIFIC_PROFILER_NAME_HERE} implementation
-	 *
-	 * @return array - array with the structure [generic_flag => specific_profiler_flag],
-	 *                                      e.g. [ProfilingFlags::CPU => XHPROF_FLAGS_CPU]
-	 */
-	public function getProfileFlagMap();
+    /**
+     * Map generic Xhgui\Profiler\ProfilingFlags to {SPECIFIC_PROFILER_NAME_HERE} implementation
+     *
+     * @return array - array with the structure [generic_flag => specific_profiler_flag],
+     *                                      e.g. [ProfilingFlags::CPU => XHPROF_FLAGS_CPU]
+     */
+    public function getProfileFlagMap();
 }
