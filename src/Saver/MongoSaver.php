dddd34ee9da27eaacd395a571ce1a841cd7c8b59
Elan Ruusamäe <glen@delfi.ee>
2020-04-19 10:41:33 +0300

    Move profiling data collection/assembling to profiler data class

--- src/Saver/MongoSaver.php
+++ src/Saver/MongoSaver.php
@@ -2,6 +2,7 @@
 
 namespace Xhgui\Profiler\Saver;
 
+use MongoDate;
 use Xhgui_Saver_Mongo;
 
 /**
@@ -16,5 +17,19 @@ class MongoSaver extends AbstractSaver
         }
 
         return class_exists('MongoClient');
+    }
+
+    public function save(array $data)
+    {
+        $this->convertTimestamps($data['meta']);
+
+        return parent::save($data);
+    }
+
+    private function convertTimestamps(array &$meta)
+    {
+        $ts = $meta['request_ts_micro'];
+        $meta['request_ts'] = new MongoDate($ts['sec']);
+        $meta['request_ts_micro'] = new MongoDate($ts['sec'], $ts['usec']);
     }
 }
