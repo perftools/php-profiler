d96c8c32d7a3c3ded89b013325ec50181ec0db7f
Elan Ruusamäe <glen@delfi.ee>
2020-04-19 12:08:04 +0300

    detect zend zts

--- install-extensions.sh
+++ install-extensions.sh
@@ -14,15 +14,13 @@ install_tideways_xhprof() {
 	local extension="tideways_xhprof"
 	local tar="$extension.tgz"
 	local config library
+	local zts
 
 	curl -fL -o "$tar" "$url"
 	tar -xvf "$tar"
 
-	env|grep -i zts
-	env|grep -i php
-	env
-	php --version|grep -i zts
-	library="$PWD/tideways_xhprof-$version/tideways_xhprof-$PHP_VERSION-zts.so"
+	zts=$(php --version | grep -q ZTS && echo -zts)
+	library="$PWD/tideways_xhprof-$version/tideways_xhprof-$PHP_VERSION$zts.so"
 	config="$HOME/.phpenv/versions/$PHP_VERSION/etc/conf.d/tideways_xhprof.ini"
 	test -f "$library"
 	echo "extension=$library" > "$config"
