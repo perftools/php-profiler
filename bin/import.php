a35259fd56f9796f4044423e5f380f2183c38955
Elan Ruusamäe <glen@pld-linux.org>
2022-05-31 22:26:00 +0300

    Allow importing multiple files or stdin

--- bin/import.php
+++ bin/import.php
@@ -7,18 +7,20 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $importer = ImporterFactory::create();
 
-if ($argc <= 1) {
-    throw new RuntimeException('Missing input filename');
+if ($argc > 1) {
+    foreach (array_splice($argv, 1) as $file) {
+        if (!is_readable($file)) {
+            throw new RuntimeException("{$file} isn't readable");
+        }
+
+        $fp = fopen($file, 'r');
+        if (!$fp) {
+            throw new RuntimeException("Can't open {$file}");
+        }
+        $importer->import($fp);
+        fclose($fp);
+    }
+} else {
+    $importer->import(STDIN);
 }
 
-$filename = $argv[1];
-if (!is_readable($filename)) {
-    throw new RuntimeException($filename . ' isn\'t readable');
-}
-
-$fp = fopen($filename, 'r');
-if (!$fp) {
-    throw new RuntimeException('Can\'t open ' . $filename);
-}
-$importer->import($fp);
-fclose($fp);
