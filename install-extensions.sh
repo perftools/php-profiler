57bcd1b5f52495886fd739faef1d0609d5ab5d33
Elan Ruusamäe <glen@pld-linux.org>
2022-06-01 10:16:39 +0300

    Change shell syntax for variable init

--- install-extensions.sh
+++ install-extensions.sh
@@ -2,9 +2,9 @@
 
 set -xeu
 
-: "${TIDEWAYS_VERSION=4.1.4}"
-: "${TIDEWAYS_XHPROF_VERSION=5.0.2}"
-: "${PHP_VERSION=7.4}"
+: "${TIDEWAYS_VERSION:=4.1.4}"
+: "${TIDEWAYS_XHPROF_VERSION:=5.0.2}"
+: "${PHP_VERSION:=7.4}"
 
 die() {
 	echo >&2 "ERROR: $*"
